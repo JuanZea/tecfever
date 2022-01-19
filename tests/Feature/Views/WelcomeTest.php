b6958199541213e18ea2c06f61d4fdd1049966ad
Juan David Zea <vikzea@gmail.com>
2022-01-19 13:15:27 -0500

    test: Welcome test added

---
+++ tests/Feature/Views/WelcomeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Views;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class WelcomeTest extends TestCase
+{
+    public function testWelcomeViewIsAccessible(): void
+    {
+        $this->get('/')->assertOk();
+    }
+
+    public function testCanBeRendered(): void
+    {
+        $view = $this->view('welcome');
+
+        $contents = (string) $this->view('welcome');
+
+        $view->assertSee('Welcome to TecFever');
+        $this->assertStringContainsString('bg-red-500', $contents);
+    }
+}
